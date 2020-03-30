cf2d697b8006d68f87f2806152d7820e887b7eb6
心叶 <yelloxing@gmail.com>
2020-03-30 18:01:52 +0800

    添加用例

---
+++ example/src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    这是主页
+    <insert-html>./cols/1.vue</insert-html>
+    </div>
+</template>
+<script>
+export default {
+  mounted() {
+    let info = "你进入的首页";
+    console.log(info);
+  }
+};
+</script>
---
+++ example/src/pages/cols/1.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>=引入的页面=</div>
+</template>
